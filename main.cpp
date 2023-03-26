0022f38aae165677833fc98e17f4a0c07eee2497
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-03-26 19:48:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    float a,b,c;
+    cout<<"inserire tre numeri reali"<<endl;
+    cin >> a >> b >> c;
+    cout<<"area triangolo="<< ((a*b)/2)<<endl;
+    cout<<"area quadrato="<<a*a<<endl;
+    cout<<"area rettangolo="<<a*b<<endl;
+    cout<<"area trapezio="<<((a+b)*c)/2<<endl;
+return 0;
 }
