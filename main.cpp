f60dbbeb1fff98bdb972254d976b8f29a301f8b8
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-03-26 19:50:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 int main(){
     float a,b,c;
-    cout<<"inserire tre numeri reali"<<endl;
     cin >> a >> b >> c;
-    cout<<"area triangolo="<< ((a*b)/2)<<endl;
-    cout<<"area quadrato="<<a*a<<endl;
-    cout<<"area rettangolo="<<a*b<<endl;
-    cout<<"area trapezio="<<((a+b)*c)/2<<endl;
+    cout<< ((a*b)/2)<<endl;
+    cout<<a*a<<endl;
+    cout<<a*b<<endl;
+    cout<<((a+b)*c)/2<<endl;
 return 0;
 }
